7de9e3878d68778507b62a9728d71f5e48387593
jeanboy <jeanboy@foxmail.com>
2017-07-27 11:42:00 +0800

    first commit.

---
+++ app/src/main/java/com/jeanboy/app/architecture/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jeanboy.app.architecture.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jeanboy.app.architecture.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
