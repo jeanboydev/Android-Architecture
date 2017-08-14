8f2b79ad68d27f4cab8e356ab372ac4361b511a8
jeanboy <caojianbo@fotoable.com>
2017-08-14 11:18:10 +0800

    fixed bug.

--- app/src/main/java/com/jeanboy/app/architecture/base/MainApplication.java
+++ app/src/main/java/com/jeanboy/app/architecture/base/MainApplication.java
@@ -2,6 +2,9 @@ package com.jeanboy.app.architecture.base;
 
 import android.app.Application;
 
+import com.jeanboy.data.cache.manager.GreenDaoHandler;
+import com.jeanboy.data.cache.manager.GreenDaoManager;
+
 /**
  * Created by jeanboy on 2017/7/27.
  */
@@ -11,5 +14,11 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        setup();
+    }
+
+    private void setup() {
+        GreenDaoManager.getInstance().build(getApplicationContext(), new GreenDaoHandler());
     }
 }
