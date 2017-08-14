066a733bc0c293cb96bbfc6bbdc50d2e2c40cd7f
jeanboy <caojianbo@fotoable.com>
2017-08-14 11:19:15 +0800

    fixed bug.

--- app/src/main/java/com/jeanboy/app/architecture/base/MainApplication.java
+++ app/src/main/java/com/jeanboy/app/architecture/base/MainApplication.java
@@ -1,6 +1,8 @@
 package com.jeanboy.app.architecture.base;
 
 import com.jeanboy.app.architecture.di.DaggerApplication;
+import com.jeanboy.data.cache.manager.GreenDaoHandler;
+import com.jeanboy.data.cache.manager.GreenDaoManager;
 
 /**
  * Created by jeanboy on 2017/7/27.
@@ -8,5 +10,14 @@ import com.jeanboy.app.architecture.di.DaggerApplication;
 
 public class MainApplication extends DaggerApplication {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
+        setup();
+    }
+
+    private void setup() {
+        GreenDaoManager.getInstance().build(getApplicationContext(), new GreenDaoHandler());
+    }
 }
